148001281c1c9ee2c70450594baee49c3ae62b2b
dchuquilla <dario.chuquilla@gmail.com>
2024-10-25 19:04:38 -0500

    Refactor: create basic html template for layout

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Links,
   Meta,
@@ -7,9 +9,14 @@ import {
 } from "@remix-run/react";
 import type { LinksFunction } from "@remix-run/node";
 
-import "./tailwind.css";
+import stylesheet from "~/tailwind.css?url";
+
+import { Navbar, Sidebar } from "flowbite-react";
+import { HiArrowSmRight, HiChartPie, HiInbox, HiShoppingBag, HiTable, HiUser, HiViewBoards } from "react-icons/hi";
+
 
 export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: stylesheet },
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
   {
     rel: "preconnect",
@@ -32,7 +39,33 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
-        {children}
+        <div className="container mx-auto">
+          <div className="flex flex-row flex-wrap py-4">
+            <main role="main" className="w-full">
+              <Navbar fluid={true} rounded={true}>
+                <Navbar.Brand href="/">
+                  <img
+                    src="/qt_logos/quien_tiene_logo.svg"
+                    className="mr-3 h-6 sm:h-9"
+                    alt="QuienTiene Logo"
+                  />
+                </Navbar.Brand>
+                <Navbar.Toggle />
+                <Navbar.Collapse>
+                  <Navbar.Link href="/navbars" active={true}>
+                    Mis Solicitudes
+                  </Navbar.Link>
+                  <Navbar.Link href="/navbars">Nosotros</Navbar.Link>
+                  <Navbar.Link href="/about">Precios</Navbar.Link>
+                  <Navbar.Link href="/navbars">Salir</Navbar.Link>
+                </Navbar.Collapse>
+              </Navbar>
+              <div className="bg-white px-2 py-2.5 dark:border-gray-700 dark:bg-gray-800 sm:px-4 rounded">
+                {children}
+              </div>
+            </main>
+          </div>
+      </div>
         <ScrollRestoration />
         <Scripts />
       </body>
