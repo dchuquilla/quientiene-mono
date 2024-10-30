26ccf68f2ddd53482199ebfd0dc81c82454f04d4
dchuquilla <dario.chuquilla@gmail.com>
2024-10-30 15:47:04 -0500

    Refactor: Update GetReplacementRequestById function to enforce ID type and improve logging

--- app/model/replacement-request.tsx
+++ app/model/replacement-request.tsx
@@ -1,13 +1,13 @@
 import { fb_db } from "./fb-initializer";
 import { collection, doc, getDoc } from 'firebase/firestore/lite';
 
-export const GetReplacementRequestById = async (id: string | undefined) => {
+export const GetReplacementRequestById = async (id: string) => {
   try {
     const replacementRequestCollection = collection(fb_db, "replacement-requests");
     const singleRequest = doc(replacementRequestCollection, id);
     const response = await getDoc(singleRequest);
     const data = response.data();
-    console.log(`Data: ${JSON.stringify(data)}`)
+    console.log(`ReplacementRequest Data: ${JSON.stringify(data)}`)
     return({data:data, id:id})
   } catch (error) {
     console.log(`Error in GetReplacementRequestById: ${error}`)
