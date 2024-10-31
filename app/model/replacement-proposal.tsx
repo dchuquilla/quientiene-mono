380723e67e5fc2d417b08e4be1d3e857186acf98
dchuquilla <dario.chuquilla@gmail.com>
2024-10-31 17:19:42 -0500

    Refactor: Integrate SaveRequestHistory in SaveReplacementProposal for tracking proposals

--- app/model/replacement-proposal.tsx
+++ app/model/replacement-proposal.tsx
@@ -1,9 +1,10 @@
-import { GetDocumentById, SaveDocument } from "./fb-initializer";
+import { GetDocumentById, SaveDocument, SaveRequestHistory } from "./fb-initializer";
 
 export const GetReplacementProposalById = async (id: string) => {
   return await GetDocumentById("replacement-proposals", id);
 };
 
 export const SaveReplacementProposal = async (data: object) => {
-  return await SaveDocument("replacement-proposals", data);
+  const proposal_id = await SaveDocument("replacement-proposals", data);
+  return await SaveRequestHistory({ "type": "replacement-proposal", "proposal_id": proposal_id });
 }
