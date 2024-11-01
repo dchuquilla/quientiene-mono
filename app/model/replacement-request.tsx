06b69ab5035e0573832712f01b75fb3dde2b5926
dchuquilla <dario.chuquilla@gmail.com>
2024-11-01 17:17:59 -0500

    Refactor: Add replacementStatusOptions for managing proposal statuses

--- app/model/replacement-request.tsx
+++ app/model/replacement-request.tsx
@@ -3,3 +3,10 @@ import { GetDocumentById } from "./fb-initializer";
 export const GetReplacementRequestById = async (id: string) => {
   return await GetDocumentById("replacement-requests", id);
 };
+
+export const replacementStatusOptions: { [key: string]: string } = {
+  "new-proposal": "Nueva propuesta",
+  "accepted-proposal": "Propuesta aceptada",
+  "rejected-proposal": "Propuesta rechazada",
+  "completed": "Completado"
+};
