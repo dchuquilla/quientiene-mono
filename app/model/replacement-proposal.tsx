00f823596c520174e6897cf0cdbbb514b5140c0e
dchuquilla <dario.chuquilla@gmail.com>
2024-11-01 17:17:29 -0500

    Refactor: Update SaveReplacementProposal to use ReplacementProposal interface and enhance history tracking

--- app/model/replacement-proposal.tsx
+++ app/model/replacement-proposal.tsx
@@ -1,10 +1,22 @@
 import { GetDocumentById, SaveDocument, SaveRequestHistory } from "./fb-initializer";
 
+interface ReplacementProposal {
+    description: string;
+    price: number;
+    photo: string;
+    request_id: string;
+  }
+
 export const GetReplacementProposalById = async (id: string) => {
   return await GetDocumentById("replacement-proposals", id);
 };
 
-export const SaveReplacementProposal = async (data: object) => {
+export const SaveReplacementProposal = async (data: ReplacementProposal) => {
   const proposal_id = await SaveDocument("replacement-proposals", data);
-  return await SaveRequestHistory({ "type": "replacement-proposal", "proposal_id": proposal_id });
+  const historyData = {
+    "type": "new-proposal",
+    "proposal_id": proposal_id,
+    "request_id": data.request_id
+  }
+  return await SaveRequestHistory(historyData);
 }
