922130f664ebd06249f00d722a5ba71f51d74d50
dchuquilla <dario.chuquilla@gmail.com>
2024-11-01 19:09:38 -0500

    Refactor: Make fields in ReplacementProposalType optional and update SaveReplacementProposal to handle missing request_id

--- app/model/replacement-proposal.tsx
+++ app/model/replacement-proposal.tsx
@@ -2,14 +2,14 @@ import { GetDocumentById, SaveDocument, SaveRequestHistory } from "./fb-initiali
 import { UpdateReplacementRequestStatus } from "./replacement-request";
 
 export interface ReplacementProposalType {
-  description: string;
-  price: number;
-  photo: string;
-  request_id: string;
-  status: string;
-  created_at: Date;
-  updated_at: Date;
-  completed_at: Date;
+  description?: string;
+  price?: number;
+  photo?: string;
+  request_id?: string;
+  status?: string;
+  created_at?: Date;
+  updated_at?: Date;
+  completed_at?: Date;
 }
 
 export const GetReplacementProposalById = async (id: string) => {
@@ -17,8 +17,11 @@ export const GetReplacementProposalById = async (id: string) => {
 };
 
 export const SaveReplacementProposal = async (data: ReplacementProposalType) => {
+  data = { ...data, "created_at": new Date(), "updated_at": new Date() };
   const proposal_id = await SaveDocument("replacement-proposals", data);
-  UpdateReplacementRequestStatus(data.request_id, "in-progress");
+  if (data.request_id) {
+    UpdateReplacementRequestStatus(data.request_id, "in-progress");
+  }
   const historyData = {
     "type": "new-proposal",
     "proposal_id": proposal_id,
