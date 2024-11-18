0d2d924464ac6938077801409b5ee42f4a5bba63
dchuquilla <dario.chuquilla@gmail.com>
2024-11-18 17:00:44 -0500

    style: Update layout for replacement requests page to improve responsiveness and structure

--- app/routes/replacement-requests.$requestId.tsx
+++ app/routes/replacement-requests.$requestId.tsx
@@ -44,8 +44,8 @@ export default function ReplacementRequests() {
       </h1>
 
       <div className="overflow-x-auto">
-        <div className="flex">
-          <div className="w-1/3">
+        <div className="flex flex-col md:flex-row">
+          <div className="w-full md:w-1/3">
             <Card href="#" className="max-w-sm">
               <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                 {replacementRequest.data?.replacement.toUpperCase()}
@@ -55,57 +55,57 @@ export default function ReplacementRequests() {
               <p className="font-normal text-gray-700 dark:text-gray-400">
                 {replacementRequest.data?.transcription} <br /><br />
                 <List>
-                  <List.Item icon={HiCheckCircle}><b>Marca:</b>&nbsp; {replacementRequest.data?.brand}</List.Item>
-                  <List.Item icon={HiCheckCircle}><b>Modelo:</b>&nbsp; {replacementRequest.data?.model}</List.Item>
-                  <List.Item icon={HiCheckCircle}><b>Año:</b>&nbsp; { replacementRequest.data?.year }</List.Item>
-                  {replacementRequest.data?.audio ? (
-                    <List.Item icon={HiCheckCircle}>
-                      <a href={replacementRequest.data?.audio} target="_blank" rel="noreferrer noopener">Audio</a>
-                    </List.Item>
-                  ): ("")}
+              <List.Item icon={HiCheckCircle}><b>Marca:</b>&nbsp; {replacementRequest.data?.brand}</List.Item>
+              <List.Item icon={HiCheckCircle}><b>Modelo:</b>&nbsp; {replacementRequest.data?.model}</List.Item>
+              <List.Item icon={HiCheckCircle}><b>Año:</b>&nbsp; { replacementRequest.data?.year }</List.Item>
+              {replacementRequest.data?.audio ? (
+                <List.Item icon={HiCheckCircle}>
+                  <a href={replacementRequest.data?.audio} target="_blank" rel="noreferrer noopener">Audio</a>
+                </List.Item>
+              ): ("")}
                 </List>
               </p>
               {user ? (
                 <Button as={Link} href={`/quotes/${replacementRequest.id}/new`} to={`/quotes/${replacementRequest.id}/new`}>
-                  Cotizar
-                  <svg className="-mr-1 ml-2 h-4 w-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                    <path
-                      fillRule="evenodd"
-                      d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
+              Cotizar
+              <svg className="-mr-1 ml-2 h-4 w-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  fillRule="evenodd"
+                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                  clipRule="evenodd"
+                />
+              </svg>
                 </Button>
               ) : null}
             </Card>
           </div>
-          <div className="w-2/3">
-            <h2 className="mb-1 text-base font-semibold text-gray-900 dark:text-white">Historial</h2>
-            <p className="flex items-center text-sm font-normal text-gray-500 dark:text-gray-400">
-              <Timeline>
-                <Timeline.Item>
-                  <Timeline.Point icon={HiCalendar} />
-                  <Timeline.Content>
-                    <Timeline.Time>{new Date(replacementRequest.data?.created_at.seconds * 1000).toLocaleDateString("en-GB")}</Timeline.Time>
-                    <Timeline.Title>{ replacementStatusOptions["new"]?.label }</Timeline.Title>
-                  </Timeline.Content>
-                </Timeline.Item>
-                {replacementRequestHistory.map((history, index) => (
-                  <Timeline.Item key={index}>
-                    <Timeline.Point icon={HiCalendar} />
-                    <Timeline.Content>
-                      <Timeline.Time>{ new Date(history.data?.created_at.seconds * 1000).toLocaleDateString("en-GB") }</Timeline.Time>
-                      <Timeline.Title>{replacementStatusOptions[history.data?.type]?.label}</Timeline.Title>
-                      {user ? (
-                        <Button color="success" as={Link} href={`/replacement-proposals/${history.data?.proposal_id}`} to={`/replacement-proposals/${history.data?.proposal_id}`}>
-                          Ver detalles
-                        </Button>
-                      ) : null}
-                    </Timeline.Content>
-                  </Timeline.Item>
-                ))}
-              </Timeline>
-            </p>
+          <div className="w-full md:w-2/3">
+        <h2 className="mb-1 text-base font-semibold text-gray-900 dark:text-white">Historial</h2>
+        <p className="flex items-center text-sm font-normal text-gray-500 dark:text-gray-400">
+          <Timeline>
+            <Timeline.Item>
+          <Timeline.Point icon={HiCalendar} />
+          <Timeline.Content>
+            <Timeline.Time>{new Date(replacementRequest.data?.created_at.seconds * 1000).toLocaleDateString("en-GB")}</Timeline.Time>
+            <Timeline.Title>{ replacementStatusOptions["new"]?.label }</Timeline.Title>
+          </Timeline.Content>
+            </Timeline.Item>
+            {replacementRequestHistory.map((history, index) => (
+          <Timeline.Item key={index}>
+            <Timeline.Point icon={HiCalendar} />
+            <Timeline.Content>
+              <Timeline.Time>{ new Date(history.data?.created_at.seconds * 1000).toLocaleDateString("en-GB") }</Timeline.Time>
+              <Timeline.Title>{replacementStatusOptions[history.data?.type]?.label}</Timeline.Title>
+              {user ? (
+            <Button color="success" as={Link} href={`/replacement-proposals/${history.data?.proposal_id}`} to={`/replacement-proposals/${history.data?.proposal_id}`}>
+              Ver detalles
+            </Button>
+              ) : null}
+            </Timeline.Content>
+          </Timeline.Item>
+            ))}
+          </Timeline>
+        </p>
           </div>
         </div>
       </div>
