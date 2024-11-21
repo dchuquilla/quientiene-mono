60cde79d5de7294285917f5786d9d665e58e60ac
dchuquilla <dario.chuquilla@gmail.com>
2024-11-21 09:27:16 -0500

    feat: Add helper function to generate random 10-character strings

---
+++ app/helpers/randomStringHelper.tsx
@@ -0,0 +1,5 @@
+// Generate a random 10-character string for photoName
+export const generateRandomString = (length: number) => {
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  return Array.from({ length }, () => chars.charAt(Math.floor(Math.random() * chars.length))).join('');
+};
