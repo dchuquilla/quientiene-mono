0682c8851b68fc381edec71b251ecbbe28c42e1f
dchuquilla <dario.chuquilla@gmail.com>
2024-11-23 11:32:49 -0500

    refactor: Simplify condition for rendering approval button in ReplacementProposal component

--- app/routes/replacement-proposals.$proposalId.tsx
+++ app/routes/replacement-proposals.$proposalId.tsx
@@ -1,6 +1,6 @@
-import { ActionFunction, ActionFunctionArgs, type LoaderFunctionArgs, type MetaFunction } from "@remix-run/node";
-import { useLoaderData, Form } from "@remix-run/react";
-import { Button, Card, List } from "flowbite-react";
+import { type LoaderFunctionArgs, type MetaFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { Card, List } from "flowbite-react";
 import { HiCheckCircle } from "react-icons/hi";
 import { GetReplacementRequestById, UpdateReplacementRequestStatus } from "../model/replacement-request";
 import { GetReplacementProposalById, UpdateReplacementProposalStatus } from "../model/replacement-proposal";
@@ -75,7 +75,7 @@ export default function ReplacementProposal() {
                 </List>
               </div>
 
-              {canApprove ? (
+              {canApprove && replacementRequest ? (
                 <button
                   type="button"
                   className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
