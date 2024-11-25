8e5b406d9691d879e376ae1b1701f30b308003c0
dchuquilla <dario.chuquilla@gmail.com>
2024-11-25 17:52:12 -0500

    refactor: Enhance ReplacementRequestHistory retrieval with proposal and store data

--- app/model/replacement-request-history.tsx
+++ app/model/replacement-request-history.tsx
@@ -1,6 +1,28 @@
 import { GetDocumentsByField } from "./fb-initializer";
+import { GetReplacementProposalById } from "./replacement-proposal";
+import { GetStoreById } from "./stores";
+
+export interface ReplacementRequestHistoryType {
+  request_id?: string;
+  proposal_id?: string;
+  type?: string;
+  created_at?: Date;
+  updated_at?: Date;
+  id?: string;
+  data?: object;
+}
+
 
 export const GetReplacementRequestHistoryByRequestId = async (id: string) => {
   console.log(`GetReplacementRequestHistoryByRequestId: ${id}`);
-  return await GetDocumentsByField("request-history", "request_id", id);
+  const history_items = await GetDocumentsByField("request-history", "request_id", id);
+  if (!history_items) {
+    return [];
+  }
+  return Promise.all(history_items.map(async (item: { id: string, data: ReplacementRequestHistoryType }) => {
+    const proposal_id: string = item.data.proposal_id || '';
+    const replacementProposal = await GetReplacementProposalById(proposal_id);
+    const store = await GetStoreById(replacementProposal?.data?.store_id);
+    return {id: item.id, data: item.data, proposal: replacementProposal?.data, store: {...store?.data, id: store?.id}};
+  }));
 };
