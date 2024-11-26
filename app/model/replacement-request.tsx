7c533134bfa48bcd60f8d55efae8b65dc6731b49
dchuquilla <dario.chuquilla@gmail.com>
2024-11-26 10:55:05 -0500

    refactor: Implement search functionality for pending replacement requests and update loader to handle search queries

--- app/model/replacement-request.tsx
+++ app/model/replacement-request.tsx
@@ -1,4 +1,5 @@
-import { GetDocumentById, GetDocumentsByField, UpdateDocument } from "./fb-initializer";
+import { fb_db, GetDocumentById, GetDocumentsByField, UpdateDocument } from "./fb-initializer";
+import { collection,getDocs, query, where, or, and } from 'firebase/firestore';
 
 export interface ReplacementRequestType {
   id?: string;
@@ -34,6 +35,22 @@ export const replacementStatusOptions: { [key: string]: { badge: string, label: 
 
 export const GetAllPendingRequests = async () => {
   return await GetDocumentsByField("replacement-requests", "status", ["new", "in-progress"]);
+}
+
+export const SearchPendingRequests = async (search: string) => {
+  const requestsRef = collection(fb_db, "replacement-requests");
+  const q = query(requestsRef, and(
+    where('status', 'in', ["new", "in-progress"]),
+    or(where('replacement', '==', search),
+      where('model', '==', search),
+      where('brand', '==', search)
+    )
+  ));
+
+  const querySnapshot = await getDocs(q);
+  const documents = querySnapshot.docs.map(doc => ({ id: doc.id, data: doc.data() }));
+  console.log(`SearchPendingRequests replacement-requests: ${documents}`);
+  return documents;
 }
 
 export const GetReplacementRequestById = async (id: string) => {
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,8 +1,9 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { LoaderFunctionArgs, MetaFunction } from "@remix-run/node";
 import { Table } from "flowbite-react";
-import { GetAllPendingRequests } from "../model/replacement-request";
-import { useLoaderData } from "@remix-run/react";
+import { GetAllPendingRequests, SearchPendingRequests } from "../model/replacement-request";
+import { Form, useLoaderData, useNavigation } from "@remix-run/react";
 import type { ReplacementRequestListType } from "../model/replacement-request";
+import { useEffect } from "react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -11,13 +12,27 @@ export const meta: MetaFunction = () => {
   ];
 };
 
-export const loader = async () => {
-  const requests = await GetAllPendingRequests();
-  return requests;
+export const loader = async ({
+    request
+  }: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+  const q = url.searchParams.get("q");
+
+  const pendingRequests = q ? await SearchPendingRequests(q) :await GetAllPendingRequests();
+  return {pendingRequests, q};
 }
 
 export default function Index() {
-  const pendingRequests = useLoaderData<ReplacementRequestListType[]>();
+  const { pendingRequests, q } = useLoaderData<{ pendingRequests: ReplacementRequestListType[], q: string | null }>();
+  const navigation = useNavigation();
+  const searching = navigation.location && new URLSearchParams(navigation.location.search).has("q");
+
+  useEffect(() => {
+    const searchField = document.getElementById("search-form");
+    if (searchField instanceof HTMLInputElement) {
+      searchField.value = q || "";
+    }
+  });
 
   return (
     <div>
@@ -26,6 +41,23 @@ export default function Index() {
         Solicitudes pendientes
       </h1>
       <div className="overflow-x-auto">
+        <Form id="search-form" role="search" className="pb-4 bg-white dark:bg-gray-900">
+          <div className="relative mt-1">
+            <div className="absolute inset-y-0 rtl:inset-r-0 start-0 flex items-center ps-3 pointer-events-none">
+              <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+              </svg>
+            </div>
+            <input
+              type="search"
+              id="search-form"
+              name="q"
+              defaultValue={q || ""}
+              aria-label="Buscar solicitudes"
+              className="block pt-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder={`${searching ? "Buscando" : "Buscar en las solicitudes"}`} />
+          </div>
+        </Form>
         <Table hoverable>
           <Table.Head>
             <Table.HeadCell>Repuesto</Table.HeadCell>
